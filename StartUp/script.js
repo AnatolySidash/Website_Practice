a31e2e75e83dabe1da036b8b1f05065d23653364
Anatoly SIdash <sidash85@yandex.ru>
2022-09-09 19:46:03 +0300

    StartUP 4th page done

---
+++ StartUp/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+   loop: true,
+   slidesPerView: 4,
+   grabCursor: true,
+
+
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+});
